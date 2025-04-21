bbdbeaabd0d5e2a17d528fcab266e0ba1d9bc10b
user1463 <termuxh539@gmail.com>
2025-04-22 00:38:56 +0300

    Create faceok.js

---
+++ plugins/faceok.js
@@ -0,0 +1,10 @@
+const { zokou } = require('../../framework/zokou');
+
+module.exports = zokou({
+    nomCom: "fbdl",
+    categorie: "Download",
+    reaction: "👍",
+    description: "Download Facebook videos"
+}, async (dest, zk, { ms, repondre, arg }) => {
+    // Facebook download implementation
+});

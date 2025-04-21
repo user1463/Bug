031eb2d790d6b17e44c48ca5831dd7dd172fd48f
user1463 <termuxh539@gmail.com>
2025-04-22 00:37:44 +0300

    Create tiktok.js

---
+++ plugins/tiktok.js
@@ -0,0 +1,10 @@
+const { zokou } = require('../../framework/zokou');
+
+module.exports = zokou({
+    nomCom: "ttdl",
+    categorie: "Download",
+    reaction: "🎵",
+    description: "Download TikTok videos"
+}, async (dest, zk, { ms, repondre, arg }) => {
+    // TikTok download implementation
+});
